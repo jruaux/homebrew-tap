3366deb068c44d1d1b872fb54b8880ac62cbbac8
Julien Ruaux <jruaux@gmail.com>
2019-08-15 22:47:37 -0700

    added riot.rb formula

---
+++ riot.rb
@@ -0,0 +1,22 @@
+class Riot < Formula
+    desc "Redis Input/Output Tool"
+    homepage "https://github.com/Redislabs-Solution-Architects/riot"
+    version "0.8.0"
+    url "https://github.com/Redislabs-Solution-Architects/riot/releases/download/#{version}/riot-#{version}-bin.tar.gz"
+    sha256 "7738034632b870f06170727d40a81a37032017afc3c89fedd9aba986235216a4"
+  
+    depends_on :java
+  
+    def install
+      libexec.install Dir["*"]
+      bin.install_symlink "#{libexec}/riot"
+      mkdir prefix/"bash"
+  
+      system "#{bin}/riot completion-script > #{prefix}/bash/completion.bash"
+      bash_completion.install "#{prefix}/bash/completion.bash" => "riot"
+  
+      system "echo 'autoload -U +X bashcompinit && bashcompinit' > #{prefix}/bash/completion.zsh"
+      system "#{bin}/riot completion-script >> #{prefix}/bash/completion.zsh"
+      zsh_completion.install "#{prefix}/bash/completion.zsh" => "_riot"
+    end
+  end
