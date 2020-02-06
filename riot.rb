f2113e0845f6109b13c15837e2dedd9a523149c4
Julien Ruaux <jruaux@gmail.com>
2020-02-06 08:41:02 -0800

    Updated to release 1.8.11

--- riot.rb
+++ riot.rb
@@ -1,9 +1,9 @@
 class Riot < Formula
     desc "Redis Input/Output Tool"
     homepage "https://github.com/Redislabs-Solution-Architects/riot"
-    version "1.8.10"
+    version "1.8.11"
     url "https://github.com/Redislabs-Solution-Architects/riot/releases/download/v#{version}/riot-#{version}.tgz"
-    sha256 "4db4d527aded40bd5db78b54375e831d8fb0a5436760fb665dac53445e64a696"
+    sha256 "2fd2cb25bacd859fec9995a7c8813609edc42fa2bbfb18f2431cec3c597c510b"
   
     depends_on :java
   
