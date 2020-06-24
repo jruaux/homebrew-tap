a14e63a793310ec76e639f81c8ae765d8fa0ec1d
jruaux <jruaux@gmail.com>
2020-06-24 16:24:15 -0700

    Updated riot-file to release 2.0.0

--- riot-file.rb
+++ riot-file.rb
@@ -2,6 +2,7 @@ class Riot < Formula
     desc "Redis Input/Output Tool"
     homepage "https://github.com/Redislabs-Solution-Architects/riot"
     version "2.0.0"
+    module "file"
     url "https://github.com/Redislabs-Solution-Architects/riot/releases/download/v#{version}/riot-#{module}-#{version}.tgz"
     sha256 "5c68e6b6d4ce3c3262a48145fb04c976da45ca87c78f1015245b757668364ef4"
   
